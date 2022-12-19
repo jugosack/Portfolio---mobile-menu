9c7e8c7a21207905aa8327693cda0f1a1a3a22ad
jugosack <jugoslav_ackoski@yahoo.com>
2022-12-19 23:25:41 +0100

    Eslint changes_1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector('.navbar__hamb');
-const navMenu = document.querySelector('.navbar__menu');
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
-document.querySelectorAll('.navbar__menu__link').forEach(n => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-}));
+const hamburger = document.querySelector(".navbar__hamb");
+const navMenu = document.querySelector(".navbar__menu__ul");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
