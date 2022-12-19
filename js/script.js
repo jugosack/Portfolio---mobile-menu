a15a2ae8092ef791661bcb565b22b3b2d7fd91e5
hafed <hafedrs@gmail.com>
2022-12-19 23:43:18 +0200

    fixing eslint

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const hamburger = document.querySelector('.navbar__hamb');
-const navMenu = document.querySelector('.navbar__menu');
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+const hamburger = document.querySelector(".navbar__hamb");
+const navMenu = document.querySelector(".navbar__menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
-document.querySelectorAll('.navbar__menu__link').forEach((n) =>
-  n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  }));
+document.querySelectorAll(".navbar__menu__link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
