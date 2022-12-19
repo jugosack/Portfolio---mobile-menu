80ffed3ae5d3d8b9da46a2bec79a027fd7fe493a
jugosack <jugoslav_ackoski@yahoo.com>
2022-12-19 23:24:27 +0100

    Eslint changes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
-document.querySelectorAll('.navbar__menu__link').forEach((n) =>
-  n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  }));
+document.querySelectorAll('.navbar__menu__link').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
